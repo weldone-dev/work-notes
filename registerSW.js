cb3c91c255c72770a737a9bd51ef78688894b3a6
weldone-dev <weldone-dev@users.noreply.github.com>
2022-11-29 21:15:21 +0000

    deploy: 9ed336935f75b111d2546aee20187599fe1e7ac7

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/work-notes/sw.js', { scope: '/work-notes/' })})}
